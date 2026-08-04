b04001cb9db84d27de4e14ee44b0be2793baec51
ryviobali <ryviobali@gmail.com>
2026-08-04 14:22:52 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Ryvio — simple offline-friendly cache-first service worker
-const CACHE_NAME = 'ryvio-cache-v142';
+const CACHE_NAME = 'ryvio-cache-v143';
 const CORE_ASSETS = [
   './',
   './manifest.json'
