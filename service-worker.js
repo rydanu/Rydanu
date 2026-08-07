e53213c5544099cba0be5ccc5f3e2e702db83231
Rydanu <ryviobali@gmail.com>
2026-08-07 22:56:35 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Ryvio — simple offline-friendly cache-first service worker
-const CACHE_NAME = 'rydanu-cache-v156';
+const CACHE_NAME = 'rydanu-cache-v157';
 const CORE_ASSETS = [
   './',
   './manifest.json'
