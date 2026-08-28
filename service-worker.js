920f1e2fa5b3ceebe1ac41de42bb4957c3663155
Rydanu <info@rydanu.com>
2026-08-28 23:55:43 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Rydanu — simple offline-friendly cache-first service worker
-const CACHE_NAME = 'rydanu-cache-v360';
+const CACHE_NAME = 'rydanu-cache-v361';
 const CORE_ASSETS = [
   './',
   './manifest.json'
